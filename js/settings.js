847ec77cd1d3e5aca1741388c5c530b24acac61d
ThreeMonthAgo <225839283+ThreeMonthAgo@users.noreply.github.com>
2026-01-13 19:08:04 +0800

    improved style

--- js/settings.js
+++ js/settings.js
@@ -12,3 +12,19 @@ const Settings =
     ],
     "ThreeMonthAgo's Blog" // title
 ];
+
+// Set the style of zero-md
+window.ZeroMd = {
+    config: {
+        cssUrls: [
+            'style/zeromdStyle.css'
+        ]
+    }
+};
+
+// set icon
+const link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = Settings[0];
+document.head.appendChild(link);
